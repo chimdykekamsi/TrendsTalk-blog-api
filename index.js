41c341543c433c8df9f3a3d6c676967c91999e7a
chimdykekamsi <ckamsi04@gmail.com>
2024-03-05 13:10:06 +0100

    enabled cors setting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: "*",
     methods: 'GET,HEAD,PUT,POST,DELETE',
     credentials: true,
 }));
